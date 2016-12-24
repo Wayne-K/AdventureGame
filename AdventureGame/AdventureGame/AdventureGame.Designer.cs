f3ee7f8675ed191548a5bcb6827e6bec81c9fefc
Wayne Kung <waynekung2@hotmail.com>
2016-12-24 14:59:51 -0500

    Added labels for Hit Points, Gold, Experience, and Level text, as well as correspdonding value labels.

--- AdventureGame/AdventureGame/AdventureGame.Designer.cs
+++ AdventureGame/AdventureGame/AdventureGame.Designer.cs
@@ -28,20 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelHP = new System.Windows.Forms.Label();
+            this.labelGold = new System.Windows.Forms.Label();
+            this.labelExperience = new System.Windows.Forms.Label();
+            this.labelLevel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hit Points:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gold:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Experience:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Level:";
+            // 
+            // labelHP
+            // 
+            this.labelHP.AutoSize = true;
+            this.labelHP.Location = new System.Drawing.Point(110, 19);
+            this.labelHP.Name = "labelHP";
+            this.labelHP.Size = new System.Drawing.Size(0, 13);
+            this.labelHP.TabIndex = 4;
+            // 
+            // labelGold
+            // 
+            this.labelGold.AutoSize = true;
+            this.labelGold.Location = new System.Drawing.Point(110, 45);
+            this.labelGold.Name = "labelGold";
+            this.labelGold.Size = new System.Drawing.Size(0, 13);
+            this.labelGold.TabIndex = 5;
+            // 
+            // labelExperience
+            // 
+            this.labelExperience.AutoSize = true;
+            this.labelExperience.Location = new System.Drawing.Point(110, 73);
+            this.labelExperience.Name = "labelExperience";
+            this.labelExperience.Size = new System.Drawing.Size(0, 13);
+            this.labelExperience.TabIndex = 6;
+            // 
+            // labelLevel
+            // 
+            this.labelLevel.AutoSize = true;
+            this.labelLevel.Location = new System.Drawing.Point(110, 99);
+            this.labelLevel.Name = "labelLevel";
+            this.labelLevel.Size = new System.Drawing.Size(0, 13);
+            this.labelLevel.TabIndex = 7;
             // 
             // AdventureGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 651);
+            this.Controls.Add(this.labelLevel);
+            this.Controls.Add(this.labelExperience);
+            this.Controls.Add(this.labelGold);
+            this.Controls.Add(this.labelHP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "AdventureGame";
             this.Text = "GameWindow";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelHP;
+        private System.Windows.Forms.Label labelGold;
+        private System.Windows.Forms.Label labelExperience;
+        private System.Windows.Forms.Label labelLevel;
     }
 }
 
