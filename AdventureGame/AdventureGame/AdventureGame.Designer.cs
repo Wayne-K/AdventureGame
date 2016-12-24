bce26db6b78847fac40b788339cbb13fac024323
Wayne Kung <waynekung2@hotmail.com>
2016-12-24 14:27:41 -0500

    Renamed window title and modified window size

--- AdventureGame/AdventureGame/AdventureGame.Designer.cs
+++ AdventureGame/AdventureGame/AdventureGame.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AdventureGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(719, 651);
+            this.Name = "AdventureGame";
+            this.Text = "GameWindow";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
