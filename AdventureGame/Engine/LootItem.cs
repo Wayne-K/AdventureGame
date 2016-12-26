5c6589adfcfafef4f9d319ee62d07a6a0ddde9f6
Wayne Kung <waynekung2@hotmail.com>
2016-12-26 05:22:23 -0500

    Added the PlayerQuest, InventoryItem, LootItem, QuestCompletionItem classes

---
+++ AdventureGame/Engine/LootItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class LootItem
+    {
+        public Item Details { get; set; }
+        public int DropPercentage { get; set; }
+        public bool isDefaultItem { get; set; }
+
+        public LootItem(Item itemDetails, int itemDropPercentage, bool itemIsDefaultItem)
+        {
+            Details = itemDetails;
+            DropPercentage = itemDropPercentage;
+            isDefaultItem = itemIsDefaultItem;
+        }
+    }
+}
