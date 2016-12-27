e3370cbdfd0d4c89c767d38342b4132718050e4d
Wayne Kung <waynekung2@hotmail.com>
2016-12-27 04:02:07 -0500

    Added Click Events for directional buttons

--- AdventureGame/AdventureGame/AdventureGame.cs
+++ AdventureGame/AdventureGame/AdventureGame.cs
@@ -28,5 +28,25 @@ namespace AdventureGame
             labelExperience.Text = player.experiencePoints.ToString();
             labelLevel.Text = player.level.ToString();
         }
+
+        private void btnNorth_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEast_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWest_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSouth_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
